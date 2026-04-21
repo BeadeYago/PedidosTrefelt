d89608e0cbcc3fe70be298566f77be0289f51f66
BeadeYago <yagito2001@gmail.com>
2026-04-21 19:54:07 -0300

    project init

---
+++ src/main/java/com/pedidos/trefelt/TrefeltApplication.java
@@ -0,0 +1,13 @@
+package com.pedidos.trefelt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrefeltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrefeltApplication.class, args);
+	}
+
+}
